f076a2023610c692b8baf660f29d8b2296100c84
jinwoo <inx0930@gmail.com>
2019-08-23 10:30:42 +0900

    props 개념 이해

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import * as serviceWorker from './serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('potato'));
+//App은 컴포넌트 모든것이 컴포넌트!
+//컴포넌트? html을 반환하는데 함수야 쉽지? jsx=> react의 고유한 개념 메이비?
+// 한 번에 하나의 컴포넌트만 렌더할수 있어 그렇기때문에 여러 컴포넌트를 쓰려면 컴포넌트안에 컴포넌트
+
+//데이터 전달 가능
+
+
+// serviceWorker.unregister();

61882b8357a6b45d2c430dfaa09f806494ab221f
jinwoo <inx0930@gmail.com>
2019-08-23 10:45:45 +0900

    반복작업 편하게

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React from 'react';
 
 
-function Food({favorite}){
-  return <h1>I like {favorite}</h1>
+function Food({name,pic}){
+  return (
+    <div>
+    <h1>I like {name}</h1>
+    <img src={pic}></img>
+    </div>
+    )
 
 }
+
+
+const foodILike = [
+  {
+    name: "김치",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "삼겹살",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "비빔밥",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "돈까스",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "김밥",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
+
+
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food favorite="kimchi" />
-      <Food favorite="ramen" />
-      <Food favorite="삼겹살" />
-      <Food favorite="밥" />
-      <Food favorite="kimchi" />
+      {foodILike.map(v=>(
+        <Food name={v.name} pic={v.image} />
+      ))}
+      
       
     </div>
   )
